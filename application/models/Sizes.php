a7237ff437b6cc711365ff15ed2b2d2497994a4c
rshellborn <rachel@shellborn.com>
2017-02-15 09:55:00 -0800

    setting up database and models for autoload, added 6 basic models, removed starter-data.sql file

---
+++ application/models/Sizes.php
@@ -0,0 +1,10 @@
+<?php
+
+class Sizes extends MY_Model {
+
+    public function __construct()
+    {
+        parent::__construct('sizes', 'id');
+    }
+
+}
---
+++ application/models/Tasks.php
@@ -0,0 +1,10 @@
+<?php
+
+class Tasks extends MY_Model {
+
+    public function __construct()
+    {
+        parent::__construct('tasks', 'id');
+    }
+
+}
